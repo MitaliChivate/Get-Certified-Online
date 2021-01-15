1b8057c6f4ce7789246875142386c402aac9547a
Mrunal Badole <mrunalb1998@gmail.com>
2021-01-15 13:59:25 +0530

    Service and controller changed for exception

--- Backend/TrainingProgram/src/main/java/com/cg/service/TrainingProgramImpl.java
+++ Backend/TrainingProgram/src/main/java/com/cg/service/TrainingProgramImpl.java
@@ -1,13 +1,18 @@
 package com.cg.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.naming.NameNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import com.cg.beans.TrainingProgram;
 import com.cg.dao.TrainingProgramDao;
+import com.cg.exception.NoValueFoundException;
 import com.cg.exception.NotPossibleException;
 
 @Service
@@ -38,21 +43,16 @@ public class TrainingProgramImpl implements TrainingProgramInterface {
 
 	}
 
-	/*
-	 * @Override public TrainingProgram searchTrainingProgram(Long
-	 * trainingProgramId) {
-	 * 
-	 * return this.trainingDao.findAll().stream().filter(x->x.getTrainingProgramId()
-	 * == trainingProgramId).findAny().get();
-	 * 
-	 * }
-	 */
-
-
 	@Override
 	public List<TrainingProgram> GetAllTrainingProgram() {
 
-		return this.trainingDao.findAll();
+		
+		 List<TrainingProgram> trainingList=new ArrayList<>();
+		 trainingList = trainingDao.findAll();
+	      if(trainingList.isEmpty())
+	  			throw new NoValueFoundException("List of trainings not found");
+	  		else
+	  			return trainingList;
 	}
 
 	@Override
@@ -64,13 +64,24 @@ public class TrainingProgramImpl implements TrainingProgramInterface {
 	@Override
 	public List<TrainingProgram> searchTrainingProgramById(Long trainingProgramId) {
 		
-			return this.trainingDao.findAll().stream().filter(x->x.getTrainingProgramId().equals(trainingProgramId)).collect(Collectors.toList());
+			
+			 List<TrainingProgram> trainingList=new ArrayList<>();
+			 trainingList = trainingDao.findAll().stream().filter(x->x.getTrainingProgramId().equals(trainingProgramId)).collect(Collectors.toList());
+		      if(trainingList.isEmpty())
+		  			throw new NoValueFoundException("TrainingProgram with trainingProgram Id:" + trainingProgramId + " does not exist");
+		  		else
+		  			return trainingList;
 		}
 
 	@Override
 	public List<TrainingProgram> searchTrainingProgramByCourse(String trainingCourse) {
 		// TODO Auto-generated method stub
-	 return this.trainingDao.findAll().stream().filter(x->x.getTrainingCourse().equals(trainingCourse)).collect(Collectors.toList());
-	}
+		 List<TrainingProgram> trainingList=new ArrayList<>();
+		 trainingList = trainingDao.findAll().stream().filter(x->x.getTrainingCourse().equals(trainingCourse)).collect(Collectors.toList());
+	      if(trainingList.isEmpty())
+	  			throw new NoValueFoundException("TrainingProgram with trainingCourse:" + trainingCourse + " does not exist");
+	  		else
+	  			return trainingList;
+		}
 
 }
