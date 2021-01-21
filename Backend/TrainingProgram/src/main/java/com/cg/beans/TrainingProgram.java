0b1b30c6586e59efd9b568f7510eeba10f7284a3
Mrunal Badole <mrunalb1998@gmail.com>
2021-01-21 12:10:38 +0530

    User and taining validations

--- Backend/TrainingProgram/src/main/java/com/cg/beans/TrainingProgram.java
+++ Backend/TrainingProgram/src/main/java/com/cg/beans/TrainingProgram.java
@@ -6,6 +6,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,12 +29,19 @@ public class TrainingProgram {
 	@SequenceGenerator(name = "training_id_sequence", initialValue = 200000, allocationSize = 1)
 	private Long trainingProgramId;
 
+	@NotBlank(message = "Training Course can not be empty")
+	@Size(min = 2, max = 200, message = "Enter a valid Course name")
 	private String trainingCourse;
 
+	@NotBlank(message = "Description can not be empty")
+	@Size(min = 2, max = 200, message = "Enter a valid Description")
 	private String description;
 	
+	@NotNull(message = "Cost cannot be null")
+	@Min(value = 99, message = "Training Cost can't be less than 99")
 	private Integer trainingCost;
 	
+	@NotNull(message = "Days cannot be empty")
 	private Integer noOfDays;
 
 }
